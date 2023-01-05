e7736b5813cf9ceaf396d2200daae28cea775a0d
Vinicius Prudencio <vynnyprudencio@gmail.com>
2023-01-05 12:39:22 -0300

    feat: add no-cache middleware on swagger api

--- apps/naruto-account-users/src/infra/external/swagger/handler/swagger.ts
+++ apps/naruto-account-users/src/infra/external/swagger/handler/swagger.ts
@@ -1,7 +1,8 @@
-import { serve, setup } from 'swagger-ui-express'
 import swaggerConfig from '@infra/external/swagger/docs'
+import { noCacheMiddleware } from '@main/http/middlewares/NoCacheMiddleware'
+import { serve, setup } from 'swagger-ui-express'
 import { Express } from 'express'
 
 export default (app: Express): void => {
-  app.use('/api-docs', serve, setup(swaggerConfig))
+  app.use('/api-docs', noCacheMiddleware, serve, setup(swaggerConfig))
 }
